cfbd30c4e56ef23a45148e568be5b92c4ef148ad
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2018-08-29 22:30:12 +0530

    Initial Commit

---
+++ MyQuotes/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyQuotes
+//
+//  Created by Pulkit Aggarwal on 29/08/18.
+//  Copyright © 2018 Maxx. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
