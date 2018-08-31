a9dccdc87450c0d5f637e6800b6d5c64f3813c05
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2018-08-31 13:45:48 +0530

    UserDefaults Completed (Shared Preferences for saving small data)

---
+++ MyQuotes/FavouriteViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FavouriteViewController.swift
+//  MyQuotes
+//
+//  Created by Pulkit Aggarwal on 31/08/18.
+//  Copyright © 2018 Maxx. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController {
+    @IBOutlet weak var favouriteImgView: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let savingDefaults = UserDefaults.standard
+        let indexSaved = savingDefaults.integer(forKey: "favourite")
+        
+        let imageName = "quote\(indexSaved)"
+        let quoteImage = UIImage(named: imageName)
+        favouriteImgView.image = quoteImage
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+
+    }
+
+
+}
---
+++ MyQuotes/HomeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  HomeViewController.swift
+//  MyQuotes
+//
+//  Created by Pulkit Aggarwal on 30/08/18.
+//  Copyright © 2018 Maxx. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    var quoteArray = ["quote0","quote1","quote2","quote3","quote4","quote5","quote6","quote7","quote8","quote9"]
+    var arrayIndex = 0
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBOutlet weak var quoteImgView: UIImageView!
+    
+    
+    @IBAction func favouriteClicked(_ sender: Any) {
+        let savingDefaults = UserDefaults.standard
+        savingDefaults.set(arrayIndex, forKey: "favourite")
+    }
+    @IBAction func previousclicked(_ sender: Any) {
+        if arrayIndex > 0
+        {
+            arrayIndex -= 1
+            
+            let imageName = quoteArray[arrayIndex]
+            let quoteImage = UIImage(named: imageName)
+            quoteImgView.image = quoteImage
+        }
+    }
+    
+    @IBAction func nextclicked(_ sender: Any) {
+        
+        if arrayIndex<quoteArray.count-1
+        {
+            arrayIndex += 1
+            
+            let imageName = quoteArray[arrayIndex]
+            let quoteImage = UIImage(named: imageName)
+            quoteImgView.image = quoteImage
+        }
+    }
+}
